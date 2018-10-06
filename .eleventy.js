751544d83a2284dfa0f2a38426a0114761cd3a7b
wookkii <wookkii@home>
2018-10-06 10:15:47 +1000

    Use slick slider. Add more slider images. Add captions

--- .eleventy.js
+++ .eleventy.js
@@ -54,6 +54,7 @@ module.exports = function(eleventyConfig) {
 
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("static/img");
+  eleventyConfig.addPassthroughCopy("static/fonts");
   eleventyConfig.addPassthroughCopy("admin");
 
   /* Markdown Plugins */
